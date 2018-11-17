1ecc3375a88e5299ddba0e9ba16c8fad9e523374
Your Name <you@example.com>
2018-11-17 11:31:33 +0100

    Consegna Verifica

--- code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
@@ -1,7 +1,7 @@
 
 // TODO Inserire il package corretto
 // nota: potete usare il suggerimento della lampadina a sinistra (scegliete l'unica opzione sensata). 
-package __TODO__;
+package it.marconicivitavecchia.as2018_19.c5Ainf.verifica;
 
 import javax.swing.*;
 
@@ -12,7 +12,7 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(250,500);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
 		
